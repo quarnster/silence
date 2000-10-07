20700d82c9194e54c8c74220fe748c75431a1f56
Fredrik Ehnbom <quarnster@gmail.com>
2000-10-07 13:55:07 +0000

    Fixed to read in the data correctly. Fixed to play the .xm correctly.

--- silence/format/xm/Xm.java
+++ silence/format/xm/Xm.java
@@ -26,30 +26,40 @@ import org.gjt.fredde.silence.format.AudioFormat;
  * The general xm class
  *
  * @author Fredrik Ehnbom
- * @version $Id: Xm.java,v 1.2 2000/10/01 17:09:31 fredde Exp $
+ * @version $Id: Xm.java,v 1.3 2000/10/07 13:55:07 fredde Exp $
  */
 public class Xm
 	extends AudioFormat
 {
 	private String title = "";
 	private String tracker = "";
-	private int patorder[];
+	int patorder[];
 
-	int default_tempo = 0;
-	int default_bpm = 0;
+	int defaultTempo = 0;
+	int defaultBpm = 0;
 	int tempo = 0;
 
-	public int samples_per_tick;
+	int samplesPerTick;
 
-	private int playingPatternPos = 0;
-	private int playingPattern = 0;
-	private int patternPos = 0;
+	int playingPatternPos = 0;
+	int playingPattern = 0;
+	int patternPos = 0;
+
+	private int restTick = 0;
 
 	private	Pattern[]	pattern;
 	protected Instrument[]	instrument;
 	private	Channel[]	channel;
 
 	public Xm() {
+	}
+
+	public static final byte[] read(BufferedInputStream in, int len)
+		throws IOException
+	{
+		byte[] b = new byte[len];
+		for (int i = 0; i < len; i += in.read(b, i, len-i));
+		return b;
 	}
 
 	/**
@@ -63,7 +73,7 @@ public class Xm
 		readGeneralInfo(in);
 
 		for (int i = 0; i < pattern.length; i++) {
-			pattern[i] = new Pattern(channel.length, in);
+			pattern[i] = new Pattern(in);
 		}
 
 		for (int i = 0; i < instrument.length; i++) {
@@ -76,8 +86,7 @@ public class Xm
 		throws IOException
 	{
 		// "Extended Module: "
-		byte b[] = new byte[17];
-		in.read(b);
+		byte b[] = read(in, 17);
 
 		if (!(new String(b)).equalsIgnoreCase("Extended Module: ")) {
 			throw new IOException("This is not a xm file!");
@@ -85,8 +94,7 @@ public class Xm
 
 
 		// Module name
-		b = new byte[20];
-		in.read(b);
+		b = read(in, 20);
 
 		title = new String(b);
 		System.out.println("Title: " + title);
@@ -94,33 +102,26 @@ public class Xm
 		in.read();
 
 		// Tracker name
-		b = new byte[20];
-		in.read(b);
+		b = read(in, 20);
 
-		tracker = new String(b);
-		System.out.println("Tracker: " + tracker);
-
+		System.out.println("Tracker: " + new String(b));
 		System.out.println("Version: 1: " + in.read() + ", 2: " + in.read());
 
 		// Header size
-		b = new byte[4];
-		in.read(b);
+		b = read(in, 4);
 
 		// Song length (in pattern order table)
-		b = new byte[2];
-		in.read(b);
+		b = read(in, 2);
 		System.out.println("song length: " +  b[0]);
 		patorder = new int[b[0]];
 
 		// Restart position
-		b = new byte[2];
-		in.read(b);
+		b = read(in, 2);
 		System.out.println("Restart position: " + b[0]);
 
 
 		// Number of channels (2,4,6,8,10,...,32)
-		b = new byte[2];
-		in.read(b);
+		b = read(in, 2);
 		System.out.println("Number of channels: " + b[0]);
 		channel = new Channel[b[0]];
 
@@ -128,43 +129,36 @@ public class Xm
 			channel[i] = new Channel(this);
 		}
 
-
 		// Number of patterns (max 128)
-		b = new byte[2];
-		in.read(b);
+		b = read(in, 2);
 		System.out.println("Number of patterns: " + b[0]);
 		pattern = new Pattern[b[0]];
 
 		// Number of instruments (max 128)
-		b = new byte[2];
-		in.read(b);
+		b = read(in, 2);
 		instrument = new Instrument[(int) ((b[0] < 0) ? 256 + b[0] : b[0])];
 		System.out.println("Number of instruments: " + instrument.length);
 
 		// Flags: bit 0: 0 = Amiga frequency table;
 		//               1 = Linear frequency table
-		b = new byte[2];
-		in.read(b);
+		b = read(in, 2);
 
 		// Default tempo
-		b = new byte[2];
-		in.read(b);
-		default_tempo = (int) b[0];
+		b = read(in, 2);
+		defaultTempo = (int) b[0];
 		System.out.println("Default tempo: " + b[0]);
 
 		// Default BPM
-		b = new byte[2];
+		b = read(in, 2);
 		int t[] = new int[2];
-		in.read(b);
 
 		t[0] = (int) ((b[0] < 0 ) ? 256 + b[0] : b[0]);
 		t[1] = (int) ((b[1] < 0 ) ? 256 + b[1] : b[1]);
-		default_bpm = (t[0] << 0) + (t[1] << 8);
-		System.out.println("Default BPM: " + default_bpm);
+		defaultBpm = (t[0] << 0) + (t[1] << 8);
+		System.out.println("Default BPM: " + defaultBpm);
 
 		// Pattern order table
-		b = new byte[256];
-		in.read(b);
+		b = read(in, 256);
 
 		System.out.println("Pattern order table: ");
 		for (int i = 0; i < patorder.length; i++) {
@@ -173,44 +167,84 @@ public class Xm
 
 			patorder[i] = b[i];
 		}
-		playingPattern = patorder[0];
+		playingPatternPos = 0;
+		playingPattern = patorder[playingPatternPos];
 	}
 
 	/**
 	 * Play...
 	 */
 	public int read(int[] buffer, int off, int len) {
+		int realLen = len;
 		for (int i = off; i < off+len; i++) {
 			buffer[i] = 0;
 		}
-		for (int i = 0; i < channel.length; i++)  {
-			channel[i].play(buffer, off, samples_per_tick);
+
+		if (restTick > 0) {
+			int read = restTick > len ? len : restTick;
+			for (int j = 0; j < channel.length; j++)  {
+				channel[j].play(buffer, off, read);
+			}
+			off += read;
+			len -= read;
+			restTick -= read;
+		}
+			
+
+readLoop:
+		for (int i = off; i < off+len; i += samplesPerTick) {
+			if (--tempo <= 0) {
+				for (int j = 0; j < channel.length; j++)  {
+					patternPos = channel[j].update(pattern[playingPattern], patternPos);
+					if (patternPos < 0) {
+						patternPos = 0;
+						tempo = 0;
+
+						continue readLoop;
+					}
+				}
+
+				if (patternPos == pattern[playingPattern].data.length) {
+					patternPos = 0;
+					playingPatternPos++;
+					if (playingPatternPos == patorder.length) {
+						// TODO: end.....
+						playingPatternPos = 0;
+					}
+					playingPattern = patorder[playingPatternPos];
+					System.out.println("Pattern: " + playingPattern);
+				}
+				tempo = defaultTempo;
+			}
+			int read = samplesPerTick;
+			if (read > off+len-i) {
+				read = off+len-i;
+				restTick = samplesPerTick - read;
+			}
+
+			for (int j = 0; j < channel.length; j++)  {
+				channel[j].updateTick();
+				channel[j].play(buffer, i, read);
+			}
 		}
 
-		if (--tempo <= 0) {
-			for (int i = 0; i < channel.length; i++)  {
-				patternPos = channel[i].update(pattern[playingPattern], patternPos);
-			}
-			if (patternPos == pattern[playingPattern].data.length) {
-				patternPos = 0;
-				playingPatternPos++;
-				playingPattern = patorder[playingPatternPos];
-				System.out.println("Pattern: " + playingPattern);
-			}
-			tempo = default_tempo;
-		}
-		return samples_per_tick;
+		return realLen;
 	}
 
 	public void setDevice(org.komplex.audio.AudioOutDevice device) {
 		super.setDevice(device);
-		samples_per_tick = (5 * deviceSampleRate) / (2 * default_bpm);
-		System.out.println("samples_per_tick: " + samples_per_tick);
+		samplesPerTick = (5 * deviceSampleRate) / (2 * defaultBpm);
+		System.out.println("samples_per_tick: " + samplesPerTick);
+		System.out.println("deviceSampleRate: " + deviceSampleRate);
 	}
 }
 /*
  * ChangeLog:
  * $Log: Xm.java,v $
+ * Revision 1.3  2000/10/07 13:55:07  fredde
+ * Fixed to read in the data correctly.
+ * Fixed to play the .xm correctly.
+ *
  * Revision 1.2  2000/10/01 17:09:31  fredde
  * added lots of code for acctual playback
  *
