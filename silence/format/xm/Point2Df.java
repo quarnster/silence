c94178b684e2c024bfeb683ecf2d87d69183d9df
Fredrik Ehnbom <quarnster@gmail.com>
2001-01-04 19:01:15 +0000

    initial commit

---
+++ silence/format/xm/Point2Df.java
@@ -0,0 +1,36 @@
+/* Point2Df.java - a 2d point represented by two floats
+ * Copyright (C) 2001 Fredrik Ehnbom
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.gjt.fredde.silence.format.xm;
+
+/**
+ * a 2d point represented by two floats
+ *
+ * @author Fredrik Ehnbom
+ * @version $Id: Point2Df.java,v 1.1 2001/01/04 19:01:15 fredde Exp $
+ */
+class Point2Df {
+	float x;
+	float y;
+}
+/*
+ * ChangeLog:
+ * $Log: Point2Df.java,v $
+ * Revision 1.1  2001/01/04 19:01:15  fredde
+ * initial commit
+ *
+ */
