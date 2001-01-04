2db8dcad3279f2a9f4edfe63035c70b2d112c68f
Fredrik Ehnbom <quarnster@gmail.com>
2001-01-04 18:53:46 +0000

    added ogg and the close method

--- silence/format/AudioFormat.java
+++ silence/format/AudioFormat.java
@@ -1,5 +1,5 @@
 /* AudioFormat.java - The basic AudioFormat class
- * Copyright (C) 2000 Fredrik Ehnbom
+ * Copyright (C) 2000-2001 Fredrik Ehnbom
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -26,9 +26,9 @@ import org.komplex.audio.*;
 /**
  * The basic class for AudioFormats. If you want to add support
  * for, lets say, .mp3-files this is the class to extend.
- * 
+ *
  * @author Fredrik Ehnbom
- * @version $Id: AudioFormat.java,v 1.3 2000/12/21 17:15:00 fredde Exp $
+ * @version $Id: AudioFormat.java,v 1.4 2001/01/04 18:53:46 fredde Exp $
  */
 public abstract class AudioFormat
 	implements PullAudioSource
@@ -50,6 +50,7 @@ public abstract class AudioFormat
 		// put all supported formats to the format list
 		flist.put(".xm", "org.gjt.fredde.silence.format.xm.Xm");
 		flist.put(".au", "org.gjt.fredde.silence.format.au.Au");
+		flist.put(".ogg", "org.gjt.fredde.silence.format.ogg.Ogg");
 	}
 
 	/**
@@ -67,7 +68,7 @@ public abstract class AudioFormat
 	 * use. I got the idea when I saw something simular in
 	 * <a href="http://www.sourceforge.net/projects/muhmuaudio">MuhmuAudio</a>
 	 * so greetings to Jarno.
-	 * 
+	 *
 	 * @param extension The extension for the fileformat handled
 	 * @param handler The handler for the extension
 	 */
@@ -103,6 +104,12 @@ public abstract class AudioFormat
 	public abstract void load(BufferedInputStream is) throws IOException;
 
 	/**
+	 * Cleanup resources used, ie the BufferedInputStream used for
+	 * streaming
+	 */
+	public abstract void close() throws IOException;
+
+	/**
 	 * Gets the AudioFormat for the specified format
 	 *
 	 * @param format The format we wish to get the AudioFormat for
@@ -120,7 +127,7 @@ public abstract class AudioFormat
 				System.err.println("couldn't create " + cl);
 				t.printStackTrace();
 			}
-		} 
+		}
 
 		return null;
 	}
@@ -139,6 +146,9 @@ public abstract class AudioFormat
 /*
  * ChangeLog:
  * $Log: AudioFormat.java,v $
+ * Revision 1.4  2001/01/04 18:53:46  fredde
+ * added ogg and the close method
+ *
  * Revision 1.3  2000/12/21 17:15:00  fredde
  * added protected field "channels", made load(is) public
  *
